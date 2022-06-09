8557dcf7ff4dfbcff02d2ea86dc89a87d1b62a2f
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-06-09 15:24:25 +0200

    Add Ray transformation method

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,4 @@
-use crate::tuple::Tuple4;
+use crate::{matrix::Matrix4x4, tuple::Tuple4};
 
 pub struct Ray {
     pub origin: Tuple4,
@@ -12,6 +12,16 @@ impl Ray {
 
     pub fn position(&self, t: f64) -> Tuple4 {
         self.origin + self.direction * t
+    }
+
+    pub fn transform(&self, m: Matrix4x4) -> Ray {
+        let new_origin = m * self.origin;
+        let new_direction = m * self.direction;
+
+        Ray {
+            origin: new_origin,
+            direction: new_direction,
+        }
     }
 }
 
@@ -27,5 +37,27 @@ mod tests {
         assert_eq!(r.position(1.0), Tuple4::point(3.0, 3.0, 4.0));
         assert_eq!(r.position(-1.0), Tuple4::point(1.0, 3.0, 4.0));
         assert_eq!(r.position(2.5), Tuple4::point(4.5, 3.0, 4.0));
+    }
+
+    #[test]
+    fn test_translating_a_ray() {
+        let r = Ray::new(Tuple4::point(1.0, 2.0, 3.0), Tuple4::vector(0.0, 1.0, 0.0));
+        let m = Matrix4x4::translation(3.0, 4.0, 5.0);
+
+        let r2 = r.transform(m);
+
+        assert_eq!(r2.origin, Tuple4::point(4.0, 6.0, 8.0));
+        assert_eq!(r2.direction, Tuple4::vector(0.0, 1.0, 0.0));
+    }
+
+    #[test]
+    fn test_scaling_a_ray() {
+        let r = Ray::new(Tuple4::point(1.0, 2.0, 3.0), Tuple4::vector(0.0, 1.0, 0.0));
+        let m = Matrix4x4::scaling(2.0, 3.0, 4.0);
+
+        let r2 = r.transform(m);
+
+        assert_eq!(r2.origin, Tuple4::point(2.0, 6.0, 12.0));
+        assert_eq!(r2.direction, Tuple4::vector(0.0, 3.0, 0.0));
     }
 }
