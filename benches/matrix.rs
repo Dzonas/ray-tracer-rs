2cc4df83de11fe71c7dc09de9b15a8b98c875a2c
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2024-01-29 14:58:10 +0100

    Add benchmark for Matrix4x4 det method

--- benches/matrix.rs
+++ benches/matrix.rs
@@ -5,6 +5,10 @@ fn matrix_4x4_inverse(data: &[f64; 16]) {
     Matrix4x4::new(*data).inverse();
 }
 
+fn matrix_4x4_det(data: &[f64; 16]) {
+    Matrix4x4::new(*data).det();
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Matrix4x4 inverse", |b| {
         b.iter(|| {
@@ -16,5 +20,16 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn matrix_det(c: &mut Criterion) {
+    c.bench_function("Matrix4x4 det", |b| {
+        b.iter(|| {
+            matrix_4x4_det(black_box(&[
+                5.0, 1.0, 3.0, 7.0, 5.0, 98.0, 15.0, 8.0, 9.0, 21.0, 11.0, 20.0, 13.0, 14.0, 15.0,
+                16.0,
+            ]))
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark, matrix_det);
 criterion_main!(benches);
